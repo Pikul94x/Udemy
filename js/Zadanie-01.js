6f7a12be27002cc71334ee41db02debfec810c52
Pikul94 <92268463+Pikul94@users.noreply.github.com>
2022-05-22 23:08:56 +0200

    2nd project finished

--- js/Zadanie-01.js
+++ js/Zadanie-01.js
@@ -19,5 +19,13 @@ const arrow = document.querySelector(".fas");
 const img = document.querySelector(".item1");
 
 const showImg = () => {
-    img.classList.toggle("hide")
-}
+  img.classList.toggle("hide");
+
+  if (img.classList.contains("hide")) {
+    arrow.style.transform = "rotate(180deg";
+  } else {
+      arrow.style.transform = "rotate(0deg)"
+  }
+};
+
+btn.addEventListener("click", showImg);
---
+++ js/Zadanie-02.js
@@ -0,0 +1,30 @@
+const sizeUp = document.querySelector(".sizeUp");
+const sizeDown = document.querySelector(".sizeDown");
+const colorBtn = document.querySelector(".color");
+const p = document.querySelector("div>p");
+
+let fontSize = 36;
+
+const increseFont = () => {
+  if (fontSize >= 50) return;
+  fontSize += 5;
+  p.style.fontSize = fontSize + "px";
+};
+
+const decreaseFont = () => {
+  if (fontSize < 21) return;
+  fontSize -= 5;
+  p.style.fontSize = fontSize + "px";
+};
+
+const colorChanger = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  p.style.color = `rgb(${r},${g},${b})`;
+};
+
+sizeUp.addEventListener("click", increseFont);
+sizeDown.addEventListener("click", decreaseFont);
+colorBtn.addEventListener("click", colorChanger);
